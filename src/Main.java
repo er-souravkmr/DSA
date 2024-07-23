aadae6358d4861f55ce31547cf84cb5ea86c6847
Sourav Kumar <er.souravkmr@gmail.com>
2024-07-23 16:45:43 +0530

    leetcode questions

--- src/Main.java
+++ src/Main.java
@@ -31,16 +31,17 @@ public class Main {
 //        list.insertLast(85);
 //        list.display();
 
-        CLL list = new CLL();
-        list.insert(32);
-        list.insert(33);
-        list.insert(34);
-        list.insert(35);
-        list.insert(36);
+//        CLL list = new CLL();
+//        list.insert(32);
+//        list.insert(33);
+//        list.insert(34);
+//        list.insert(35);
+//        list.insert(36);
+//
+//        list.display();
+//        list.delete(36);
+//        list.display();
 
-        list.display();
-        list.delete(36);
-        list.display();
     }
 }
 
@@ -60,4 +61,86 @@ public class Main {
 //
 //    }
 //    return head;
+//}
+
+
+//#leetcode 141 Check  whether  LL is cyclic
+//
+//public class Solution {
+//    public boolean hasCycle(ListNode head) {
+//        if(head == null || head.next == null){
+//            return false;
+//        }
+//
+//        ListNode fast = head;
+//        ListNode slow = head;
+//
+//        while(fast != null && fast.next != null && slow != null && slow.next!=null){
+//            fast = fast.next.next;
+//            slow = slow.next;
+//            if(fast == slow){
+//                return true;
+//            }
+//        }
+//
+//        return false;
+//    }
+
+
+//#Tell the length of the Cyclic nodes in LinkedList
+
+//public class Solution {
+//    public boolean hasCycle(ListNode head) {
+//        if(head == null || head.next == null){
+//            return false;
+//        }
+//
+//        ListNode fast = head;
+//        ListNode slow = head;
+//        int length = 0;
+//
+//        while(fast != null && fast.next != null ){
+//            fast = fast.next.next;
+//            slow = slow.next;
+//            if(fast == slow){
+//                 ListNode temp = slow;
+//                  do{
+//                      temp=temp.next;
+//                      length++;
+//                  }while(temp!=slow);
+//                 return length;
+//            }
+//        }
+//
+//        return length;
+//    }
+
+
+//#Happy Number
+//class Solution {
+//    public boolean isHappy(int n) {
+//        int slow = n;
+//        int fast = n;
+//
+//        do{
+//            slow = square(slow);
+//            fast = square(square(fast));
+//        }while(slow!=fast);
+//
+//        if(slow==1){
+//            return true;
+//        }
+//
+//        return false;
+//    }
+//
+//    private int square(int n){
+//        int ans = 0;
+//        while(n!=0){
+//            int d = n%10;
+//            ans = ans + d*d;
+//            n/=10;
+//        }
+//        return ans;
+//    }
 //}
